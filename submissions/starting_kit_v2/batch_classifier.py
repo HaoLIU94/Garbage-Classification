08b80507b4a3862ed0eb304ab8af7ff731979969
RobinKD <robin.duraz@gmail.com>
2019-01-29 22:11:18 +0100

    Update data + starting_kit pytorch

--- submissions/starting_kit_v2/batch_classifier.py
+++ submissions/starting_kit_v2/batch_classifier.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
+import torchvision.transforms.functional as TF
 import numpy as np
 import time
 import os
@@ -11,6 +12,42 @@ from torch.autograd import Variable
 from torchvision import transforms
 from tqdm import tqdm
 from rampwf.workflows.image_classifier import get_nb_minibatches
+from PIL import Image
+
+
+def my_transforms(image):
+    scale = 360
+    input_shape = 224
+
+    # images = []
+    image = Image.fromarray(image.astype('uint8'), 'RGB')
+    image = TF.resize(image, scale)
+    image = TF.resized_crop(image, i=68, j=68, h=input_shape, w=input_shape, size=input_shape)
+    return image
+    # images.append(image)
+    # images.append(TF.hflip(image))
+    # images.append(TF.vflip(image))
+    # images.append(TF.rotate(image, 90))
+    # images.append(TF.rotate(image, 270))
+    # return images
+
+def data_augment(inputs):
+    mean = [0.5, 0.5, 0.5]
+    std = [0.5, 0.5, 0.5]
+    res = torch.empty(inputs.shape[0], 3, 224, 224)
+    for i, img in enumerate(inputs):
+        x = my_transforms(img)
+        x = TF.to_tensor(x)
+        x = TF.normalize(x, std, mean)
+        res[i] = x
+    return res
+    # new_X = []
+    # imgs_tfm = my_transforms(inputs)
+    # for i in imgs_tfm:
+    #     img = TF.to_tensor(i)
+    #     img = TF.normalize(img, mean, std)
+    #     new_X.append(i)
+    # return new_X
 
 
 class BatchClassifier(object):
@@ -19,62 +56,19 @@ class BatchClassifier(object):
         self.epochs = 3
 
     def fit(self, gen_builder):
-        batch_size = 32
+        batch_size = 16
+
+
         gen_train, gen_valid, nb_train, nb_valid =\
             gen_builder.get_train_valid_generators(
-                batch_size=batch_size, valid_ratio=0.2)
-        self.model.fit_generator(
-            gen_train,
-            # Total number of steps (batches of samples) to yield from
-            # generator before declaring one epoch finished and starting the
-            # next epoch. It should typically be equal to the number of unique
-            # samples of your dataset divided by the batch size.
-            steps_per_epoch=get_nb_minibatches(nb_train, batch_size),
-            epochs=1,
-            # In parallel to training, a CPU process loads and preprocesses
-            # data from disk and put it into a queue in the form of
-            # mini-batches of size `batch_size`.`max_queue_size` controls the
-            # maximum size of that queue. The size of the queue should be big
-            # enough so that the training process (GPU) never
-            # waits for data (the queue should be never be empty).
-            # The CPU process loads chunks of 1024 images each time, and
-            # 1024/batch_size mini-batches from that chunk are put into the
-            # queue. Assuming training the model on those 1024/batch_size
-            # mini-batches is slower than loading a single chunk of 1024
-            # images, a good lower bound for `max_queue_size` would be
-            # (1024/batch_size). if `batch_size` is 16, you can put
-            # `max_queue_size` to 64.
-            max_queue_size=64,
-            # WARNING : It is obligatory to set `workers` to 1.
-            # This in principle controls the number of workers used
-            # by keras to load mini-batches from disk to memory in parallel
-            # to GPU training. But I don't like the way it works and their
-            # code is not very commented/used, so I dont trust it that much
-            # (we might have surprises).
-            # The way it works in keras is by launching in parallel `workers`
-            # threads or processes which will all use a copy of the generator
-            # passed to `fit_generator`. So if nothing is done and `workers`
-            # is set to some number > 1, the neural net will be trained with
-            # repetitions of the same data, because the workers are independent
-            # and they got through the same generator.
-            # Hence it is necessary to introduce a shared lock between the the
-            # processes so that they load different data, this can become a bit
-            # complicated, so I choose to rather load exactly one chunk at a
-            # time using 1 worker (so `workers` have to be equal to 1), but
-            # do this single chunk loading in parallel with joblib.
-            workers=1,
-            use_multiprocessing=True,
-            validation_data=gen_valid,
-            validation_steps=get_nb_minibatches(nb_valid, batch_size),
-            verbose=1)
+                batch_size=batch_size, valid_ratio=0.3)
 
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(list(filter(lambda p: p.requires_grad, self.model.parameters())), lr=0.001, momentum=0.9)
         scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-        alpha = 0.1
-
         since = time.time()
+        use_gpu = False
 
         # graphs
         loss_values_train = []
@@ -96,23 +90,28 @@ class BatchClassifier(object):
                     self.model.train(True)  # Set model to training mode
                 else:
                     self.model.train(False)  # Set model to evaluate mode
-                    
+
                 running_loss = 0.0
                 running_corrects = 0
 
-                # Iterate over data.
-                print(phase)
-                for data in tqdm(dataloaders[phase]):
+                gen_data = None
+                if phase == 'train':
+                    gen_data = gen_train
+                else:
+                    gen_data = gen_valid
+
+                for data in tqdm(gen_data):
                     # get the inputs
                     inputs, labels = data
-                
-                    #augementation using mixup
-                    if phase == 'train' and mixup:
-                        inputs = mixup_batch(inputs, alpha)
-                    
+
+                    inputs = data_augment(inputs)
+                    labels = torch.from_numpy(labels)
+                    labels = labels.type(torch.LongTensor)
+
+
                     if use_gpu:
                         inputs, labels = inputs.cuda(), labels.cuda()
-                
+
                     # zero the parameter gradients
                     optimizer.zero_grad()
 
@@ -121,6 +120,7 @@ class BatchClassifier(object):
                     if type(outputs) == tuple:
                         outputs, _ = outputs
                     _, preds = torch.max(outputs, 1)
+                    _, labels = torch.max(labels, 1)
                     loss = criterion(outputs, labels)
 
                         # backward + optimize only if in training phase
@@ -132,9 +132,9 @@ class BatchClassifier(object):
                     running_loss += loss.item()
                     running_corrects += int(torch.sum(preds == labels))
 
-                epoch_loss = running_loss / dataset_sizes[phase]
-                epoch_acc = running_corrects / dataset_sizes[phase]
-            
+                epoch_loss = running_loss / nb_train
+                epoch_acc = running_corrects / nb_valid
+
                 if phase == 'train':
                     loss_values_train.append(epoch_loss)
                     acc_values_train.append(epoch_acc)
@@ -145,8 +145,8 @@ class BatchClassifier(object):
                 print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
                 # deep copy the model
-                if phase == 'val' and epoch_acc > best_acc:
-                    best_acc = epoch_acc
+                # if phase == 'val' and epoch_acc > best_acc:
+                #     best_acc = epoch_acc
                 #     best_model_wts = self.model.state_dict()
 
             print()
@@ -154,14 +154,20 @@ class BatchClassifier(object):
         time_elapsed = time.time() - since
         print('Training complete in {:.0f}m {:.0f}s'.format(
             time_elapsed // 60, time_elapsed % 60))
-        print('Best val Acc: {:4f}'.format(best_acc))
+        # print('Best val Acc: {:4f}'.format(best_acc))
 
         # load best model weights
         # self.model.load_state_dict(best_model_wts)
         # return self.model, ((loss_values_train, acc_values_train), (loss_values_valid, acc_values_valid))
 
     def predict_proba(self, X):
-        return self.model.predict(X)
+        pred = self.model.predict(X)
+        preds = []
+        for p in pred:
+            p_prob = [0,0,0,0,0,0]
+            p_prob[p] = 1
+            preds.append(p_prob)
+        return preds
 
     def _build_model(self):
         model_conv = torchvision.models.resnet18(pretrained=True)
